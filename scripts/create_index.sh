f319179b0c68102f29c220707f8f248e909b142e
pierre-maraval <pierre.maraval@gmail.com>
2024-10-22 15:22:47 +0200

    Feat : ajout création index logkbart dans ES au démarrage des containers

---
+++ scripts/create_index.sh
@@ -0,0 +1,47 @@
+curl -X PUT "$1/logkbart
+{
+	"settings": {
+		"number_of_shards": 5,
+		"number_of_replicas": 0
+	},
+	"mappings": {
+		"properties": {
+			"ID": {
+				"type": "text"
+			},
+			"PACKAGE_NAME": {
+				"type": "text"
+			},
+			"TIMESTAMP": {
+				"type": "date"
+			},
+			"THREAD": {
+				"type": "text"
+			},
+			"LEVEL": {
+				"type": "text"
+			},
+			"LOGGER_NAME": {
+				"type": "text"
+			},
+			"MESSAGE": {
+				"type": "text"
+			},
+			"END_OF_BATCH": {
+				"type": "boolean"
+			},
+			"LOGGER_FQCN": {
+				"type": "text"
+			},
+			"THREAD_ID": {
+				"type": "integer"
+			},
+			"THREAD_PRIORITY": {
+				"type": "integer"
+			},
+			"NB_LINE": {
+				"type": "integer"
+			}
+		}
+	}
+}"
